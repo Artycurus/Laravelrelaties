82216a83772203a63475734173e6afd91bf0bba1
Artycurus <git>
2024-12-07 17:19:22 +0100

    video 5 af

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        relation::morphMap([
+            'series' => 'app\series',
+            'collection' => 'app\collection'
+        ])
     }
 }
